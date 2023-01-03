49e3d812aae06348301e0cc1c6473beb59935254
parkhwakyoung <firenoah.m@gmail.com>
2023-01-03 14:21:35 +0900

    기본 화면 구성

--- Animation/Animation.cpp
+++ Animation/Animation.cpp
@@ -1,28 +1,28 @@
+#include <stdio.h>
 #include <SFML/Graphics.hpp>
 
-    int main(void) {
+using namespace sf;
 
-        RenderWindow window(VideoMode(1200, 800), "AfterSchool2");// 화면 크기, 제목
-        window.setFramerateLimit(60);// 60초마다 한 번씩 돌아간다
+int main(void)
+{
+    RenderWindow window(VideoMode(1200, 800), "Animation");
+    window.setFramerateLimit(60);
 
-        // f : 소수
-        // Vector2i : 정수
-
-        while (window.isOpen())
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
         {
-          
-            Event event;
-            while (window.pollEvent(event))
+            switch (event.type)
             {
-                switch (event.type)
-                {
-                case Event::Closed:
-                    window.close();
-                    break;
-                }
+            case Event::Closed:
+                window.close();
+                break;
+            default:
+                break;
             }
         }
-
-        return 0;
     }
+
+    return 0;
 }
